183546a2fd38b9dd019979625017f12dc456933f
Woogear Kwon <techkwon1717@hanmail.net>
2023-12-06 16:52:58 +0900

    [UPDATE] AutoSizingText 크기에 따라 동적으로 텍스트 크기 적용되도록 수정

--- presentation/src/main/java/com/woogear/presentation/screen/category/compose/autosizingtext/AutoSizingTextrScreen.kt
+++ presentation/src/main/java/com/woogear/presentation/screen/category/compose/autosizingtext/AutoSizingTextrScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
@@ -19,6 +18,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Slider
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
@@ -32,19 +32,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.woogear.presentation.R
-import com.woogear.presentation.theme.backgroundColors
+import com.woogear.presentation.theme.paletteBlue070
+import com.woogear.presentation.theme.paletteYellow100
+import timber.log.Timber
 
 @Composable
 fun AutoSizingTextScreen(
     modifier: Modifier = Modifier,
     onClickExit: () -> Unit,
 ) {
+    var firstCardHeight by remember { mutableStateOf(180f) }
+
     Scaffold(
         modifier = modifier.systemBarsPadding(),
         topBar = {
@@ -65,21 +70,30 @@ fun AutoSizingTextScreen(
                 .padding(it)
                 .padding(10.dp)
         ) {
+            Slider(
+                value = firstCardHeight,
+                valueRange = 80f..220f,
+                onValueChange = { newValue ->
+                    firstCardHeight = newValue
+                },
+            )
+            Text(text = "Height = ${firstCardHeight.toInt()}dp")
+            Spacer(modifier = Modifier.height(10.dp))
             CardBox(
-                height = 180.dp,
-                background = Color(backgroundColors.random()),
+//                height = firstCardHeight.dp,
+                background = paletteYellow100,
             ) {
                 AutoSizedText(
-                    modifier = Modifier.fillMaxHeight(),
-                    style = TextStyle(fontSize = 22.sp),
+                    modifier = Modifier.height(firstCardHeight.dp),
+                    originalStyle = TextStyle(fontSize = 45.sp),
                     textColor = Color.White,
-                    text = stringResource(id = R.string.auto_sizing_text_sample_long),
+                    text = stringResource(id = R.string.auto_sizing_text_sample_short),
                 )
             }
             Spacer(modifier = Modifier.height(10.dp))
             CardBox(
-                height = 280.dp,
-                background = Color(backgroundColors.random()),
+                modifier = Modifier.height(280.dp),
+                background = paletteBlue070,
             ) {
                 Row(
                     modifier = Modifier.fillMaxWidth(),
@@ -88,7 +102,7 @@ fun AutoSizingTextScreen(
                         modifier = Modifier
                             .fillMaxHeight()
                             .weight(1f),
-                        style = TextStyle(fontSize = 18.sp),
+                        originalStyle = TextStyle(fontSize = 18.sp),
                         textColor = Color.White,
                         text = "Lorem Ipsum",
                         scaleBy = TextScaleType.WIDTH
@@ -98,7 +112,7 @@ fun AutoSizingTextScreen(
                         modifier = Modifier
                             .fillMaxHeight()
                             .weight(1.5f),
-                        style = TextStyle(fontSize = 18.sp),
+                        originalStyle = TextStyle(fontSize = 18.sp),
                         textColor = Color.White,
                         text = stringResource(id = R.string.auto_sizing_text_sample_short),
                     )
@@ -107,7 +121,7 @@ fun AutoSizingTextScreen(
                         modifier = Modifier
                             .fillMaxHeight()
                             .weight(1.5f),
-                        style = TextStyle(fontSize = 18.sp),
+                        originalStyle = TextStyle(fontSize = 18.sp),
                         textColor = Color.White,
                         text = stringResource(id = R.string.auto_sizing_text_sample_long),
                     )
@@ -120,28 +134,43 @@ fun AutoSizingTextScreen(
 @Composable
 private fun AutoSizedText(
     modifier: Modifier = Modifier,
-    style: TextStyle,
+    originalStyle: TextStyle,
     textColor: Color,
     text: String,
     scaleBy: TextScaleType = TextScaleType.HEIGHT,
 ) {
-    var textStyle by remember { mutableStateOf(style) }
+    var size by remember { mutableStateOf(IntSize.Zero) }
+    var textStyle by remember { mutableStateOf(originalStyle) }
     var readyToDraw by remember { mutableStateOf(false) }
 
     Text(
-        modifier = modifier.drawWithContent { if (readyToDraw) drawContent() },
+        modifier = modifier.drawWithContent {
+                if (readyToDraw) drawContent()
+            },
         text = text,
         maxLines = if (scaleBy == TextScaleType.HEIGHT) Int.MAX_VALUE else 1,
         color = textColor,
         style = textStyle,
         softWrap = scaleBy == TextScaleType.HEIGHT,
         onTextLayout = { textLayoutResult ->
+            val newSize = textLayoutResult.size
+
+            if (size != newSize) {
+                if (size.height < newSize.height || size.width < newSize.width) {
+                    // reset textStyle to Original style
+                    textStyle = originalStyle
+                }
+
+                size = newSize
+            }
+
             val didOverFlow =
                 if (scaleBy == TextScaleType.HEIGHT) textLayoutResult.didOverflowHeight
                 else textLayoutResult.didOverflowWidth
 
             if (didOverFlow) {
                 textStyle = textStyle.copy(fontSize = textStyle.fontSize * 0.9)
+                readyToDraw = false
             } else {
                 readyToDraw = true
             }
@@ -152,7 +181,6 @@ private fun AutoSizedText(
 @Composable
 private fun CardBox(
     modifier: Modifier = Modifier,
-    height: Dp,
     paddingValues: PaddingValues = PaddingValues(vertical = 24.dp, horizontal = 16.dp),
     background: Color,
     onClick: (() -> Unit)? = null,
@@ -161,7 +189,6 @@ private fun CardBox(
     Box(
         modifier = modifier
             .fillMaxWidth()
-            .height(height)
             .clip(shape = RoundedCornerShape(size = 24.dp))
             .background(background)
             .clickable(
